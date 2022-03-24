dee94790be90737b27f71d1735523df2a1569ccb
Weslley Rodrigo <weslley.rodriigo@gmail.com>
2022-03-24 00:17:27 -0300

    Make content headers static

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,23 @@
     </div>
     <div class="column is-three-quarter content">
          <Form @finishTask="saveTask"/>
-        <div class="list">
-            <Box v-if="emptyList">
-                You're not very productive today :(
-            </Box>
+            <template v-if="emptyList">
+                <div class="list">
+                    <Box>
+                        You're not very productive today :(
+                    </Box>
+                </div>
+            </template>
             <template v-else>
-                <div class="columns has-text-weight-bold list-header">
+                <div class="columns has-text-weight-bold list-header" style="margin-bottom: 0">
                     <div class="column is-5">Task</div>
                     <div class="column is-4">Time</div>
                     <div class="column is-3">Start</div>
                 </div>
+                <div class="list">
                 <Task v-for="(task, index) in tasks" :key="index" :task="task"/>
+                </div>
             </template>
-      </div>
     </div>
   </main>
 </template>
@@ -67,35 +71,35 @@ export default defineComponent({
 
 <style>
 html {
-  overflow: hidden;
+    overflow: hidden;
 }
 .sidebar-wrapper {
     z-index: 2;
     border-right: 1px solid #ececec;
 }
 .list {
-  padding: 1.25rem;
-  max-height: calc(100vh - 104px);
-  overflow: auto;
+    padding: 1.25rem;
+    max-height: calc(100vh - 140px);
+    overflow: auto;
 }
 .list-header {
-  padding: 0 15px;
-  color: var(--texto-primario);
+    padding: 0 2rem;
+    color: var(--texto-primario);
 }
 main {
-  --bg-primario: #fff;
-  --bg-primario--aux: #f7f7f7;
-  --bg-secundario: #fff;
-  --texto-primario: #000;
+    --bg-primario: #fff;
+    --bg-primario--aux: #f7f7f7;
+    --bg-secundario: #fff;
+    --texto-primario: #000;
 }
 main.dark-theme {
-  --bg-primario: #2a2a2a;
-  --bg-primario--aux: #262626;
-  --bg-secundario: #1b1d1e;
-  --texto-primario: #939393;
+    --bg-primario: #2a2a2a;
+    --bg-primario--aux: #262626;
+    --bg-secundario: #1b1d1e;
+    --texto-primario: #939393;
 }
 .content {
-  background-color: var(--bg-primario--aux);
+    background-color: var(--bg-primario--aux);
 }
 
 @media only screen and (max-width: 768px) {
